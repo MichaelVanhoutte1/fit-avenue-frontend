f2dec71076d7beb4509621dbb70927c7a51491a0
michael vanhoutte <michael.vanhoutte@student.howest.be>
2023-01-05 08:09:52 +0100

    refactored login screen

--- styles/pages/login.styled.ts
+++ styles/pages/login.styled.ts
@@ -16,21 +16,25 @@ export const SubTitle = styled.h1`
 
 export const SignUpButton = styled.button`
     color: ${colors.whiteText};
-    background-color: ${colors.secondary};
-    border: 2px solid ${colors.secondary} !important;
+    background: -webkit-linear-gradient(325deg, rgb(62, 73, 163) 0%, rgb(49, 58, 117) 50%);
+    background: -o-linear-gradient(325deg, rgb(62, 73, 163) 0%, rgb(49, 58, 117) 50%);
+    background: -ms-linear-gradient(325deg, rgb(62, 73, 163) 0%, rgb(49, 58, 117) 50%);
+    background: -moz-linear-gradient(325deg, rgb(62, 73, 163) 0%, rgb(49, 58, 117) 50%);
+    background: linear-gradient(125deg, rgb(62, 73, 163) 0%, rgb(49, 58, 117) 50%);
+    border: 2px solid ${colors.purple}; !important;
     width: 15rem;
     border: none;
     padding: 1.3rem 0;
     text-transform: uppercase;
     font-weight: 600;
-    margin: 0 0 1rem 0;
+    margin: 0 1.6rem 1rem 0;
     display: block;
     letter-spacing: 0.1rem;
 `;
 
 export const SignUpFacebookButton = styled.button`
-    border: 2px solid ${colors.secondary} !important;
-    color: ${colors.secondary};
+    border: 2px solid ${colors.purple} !important;
+    color: ${colors.purple};
     background-color: ${colors.whiteText};
     width: 15rem;
     border: none;
@@ -38,35 +42,36 @@ export const SignUpFacebookButton = styled.button`
     text-transform: uppercase;
     font-weight: 600;
     display: block;
-    margin: 0 0 4rem 0;
+    margin: 0 1.6rem 4rem 0;
     letter-spacing: 0.1rem;
 `;
 
 export const SignInTitle = styled.h1`
     color: ${colors.blackText};
-    text-align: center;
+    text-align: right;
     font-weight: 400;
-    margin-bottom: 1.5rem;
+    margin: 0 3rem 1.5rem 0;
 `;
 
 export const SignInButton = styled.a`
-    color: ${colors.secondary};
+    color: ${colors.purple};
     border: none;
     text-transform: uppercase;
     font-weight: 600;
     display: block;
     text-align: end;
-    margin-right: 1.6rem;
+    margin-right: 3rem;
     letter-spacing: 0.1rem;
 `;
 
 export const ActionDiv = styled.div`
     display: block;
-    margin: 0 0 0 auto;
+    margin: 27vh 0 0 auto;
     width: fit-content;
 `;
 
 export const HeadDiv = styled.div`
     display: block;
     width: fit-content;
+    margin: 10vh 0 0 2rem;
 `;
